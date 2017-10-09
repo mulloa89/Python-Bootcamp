f1d3c90af7f149b35c014a373208740bc35c2827
Mark Ulloa <m_ulloa@me.com>
2017-10-08 22:27:05 -0700

    lists.py updated

--- lists.py
+++ lists.py
@@ -6,21 +6,67 @@ print my_list
 
 print len(my_list)
 
+#Indexing & Slicing
+
 my_list2 = ['one','two', 'three', 4,5]
 
 print my_list2
 
-print my_list2[0]
+print my_list2[0] #Grab element at index 0
 
-print my_list2 [1:]
+print my_list2 [1:] #Grab index 1 & everything past it
 
-print my_list2[:3]
+print my_list2[:3] #Grab everything UP TO index 3
 
-print my_list2 + ['new item']
+print my_list2 + ['new item'] #concatenate;does NOT change original
+
+# Permanent change to my_list2
+my_list2 = my_list2 + ['add new item permanently']
+print my_list2
+
+#Make the list double;not permanent
+print my_list2*2
+
+"""Basic List Methods"""
+
+l = [1,2,3]
+print l
+
+#Append
+l.append('append me!')
+print l
+
+"""Use pop to 'pop off' an item from the list. By default pop takes off
+the last index, but you can also specify which index to pop off."""
+
+# Pop off the 0 idexed item
+print l.pop(0)
+
+# Assign the popped element, remember default popped index is -1
+popped_item = l.pop()
+
+print popped_item
+
+print l
+
+
+# We can use the sort method and the reverse methods as well
 
 new_list = ['a', 'e', 'x', 'b', 'c']
 
 print new_list
+
+# use reverse to reverse order (this is PERMANENT!)
+new_list.reverse()
+print new_list
+
+# use sort to sort the list(alphabetical or numerical/ascending)
+new_list.sort()
+print new_list
+
+""""Nesting Lists: we can have data structures...
+within data structures. For ex: A list inside a list"""
+
 
 l_1 = [1,2,3]
 l_2 = [4,5,6]
