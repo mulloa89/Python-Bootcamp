6f09b550751e941c2620680f45e1ca2434307ff9
Mark Ulloa <m_ulloa@me.com>
2017-10-11 23:37:25 -0700

    Files Notes

---
+++ files.py
@@ -0,0 +1,4 @@
+"""Files:**will probably need to install certain libraries or modules
+to interact with various file types. Python has a built-in open
+function to open and play with basic file types."""
+
