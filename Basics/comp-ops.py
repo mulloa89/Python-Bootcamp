c59ad4120466c17efef4a6af24c01610c6061ddd
Mark Ulloa <m_ulloa@me.com>
2017-10-14 19:22:52 -0700

    Updated

---
+++ Basics/comp-ops.py
@@ -0,0 +1,28 @@
+""" Comparison Operators"""
+
+""" These operators will allow us to compare variables and output
+a Boolean value (True or False). """
+
+""" Table of Comparison Operators
+
+== - if both values are equal
+!= - if values are not equal
+<> - if values are not equal
+> - greater than
+< - less than
+>= - greater than, or equal to
+<= - less than, or equal to
+
+"""
+
+2 == 2 is True
+
+1 == 0 is False
+
+2 != 1 is True
+
+2 != 2 is False
+
+2 <> 1 is True
+
+2 <> 2 is False
